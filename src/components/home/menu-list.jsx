27d722b8449c253ecd4cfeb8dfe599a0458d7b21
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 10:49:53 +0600

    style: adjust the menu item size

--- src/components/home/menu-list.jsx
+++ src/components/home/menu-list.jsx
@@ -12,15 +12,15 @@ export function MenuList({ menu_image, menu_name }) {
 			className="space-y-3 text-center"
 		>
 			<div
-				className={`w-30 md:w-50 h-30 md:h-50 ring-emerald-500 m-1 rounded-full transition duration-300 ${
+				className={`w-20 md:w-30 h-20 md:h-30 ring-emerald-500 m-1 rounded-full transition duration-300 ${
 					category === menu_name ? "ring-4 shadow-xl" : ""
 				}`}
 			>
 				<Image
 					className="w-full aspect-square"
 					src={menu_image}
-					width={200}
-					height={200}
+					width={100}
+					height={100}
 					alt={menu_name}
 				/>
 			</div>
