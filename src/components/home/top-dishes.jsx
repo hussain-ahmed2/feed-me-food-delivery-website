66b9e03072878697299e1f0a98fd128397a41467
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 22:27:13 +0600

    refactor: refactor the component to client component

--- src/components/home/top-dishes.jsx
+++ src/components/home/top-dishes.jsx
@@ -1,14 +1,11 @@
-import { Suspense } from "react";
-import DishesGrid from "./dishes-gird";
+import DishesGrid from "./top-dishes-gird";
 
 export default function TopDishes() {
 	return (
 		<section className="flex flex-col">
 			<h2 className="heading mb-8">Top dishes near you</h2>
 
-			<Suspense fallback={<div>Loading dishes...</div>}>
-				<DishesGrid />
-			</Suspense>
+			<DishesGrid />
 		</section>
 	);
 }
