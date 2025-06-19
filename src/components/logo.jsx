dbd7cf1168412424daeb680cb083a38dd6875798
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 09:25:58 +0600

    feat: create logo component

---
+++ src/components/logo.jsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+	return (
+		<div className="font-black text-2xl">
+			<span className="text-emerald-500">Feed</span>
+			<span className="text-amber-500">Me</span>
+		</div>
+	);
+}
