ac66eb5121ca95c6ab29b555367caeaac698962d
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 22:26:04 +0600

    feat: create GET /api/products route

---
+++ src/app/api/products/route.js
@@ -0,0 +1,28 @@
+import { connectDB } from "@/mongodb/connectDB";
+import Product from "@/mongodb/models/product";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+	try {
+		await connectDB();
+
+		const { searchParams } = new URL(req.url);
+		const category = searchParams.get("category");
+		const page = parseInt(searchParams.get("page")) || 1;
+		const limit = parseInt(searchParams.get("limit")) || 12;
+		const skip = (page - 1) * limit;
+
+		const query = category ? { category } : {};
+
+		const total = await Product.countDocuments(query);
+		const dishes = await Product.find(query).skip(skip).limit(limit);
+
+		return NextResponse.json({ success: true, dishes, total });
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json(
+			{ success: false, message: "Internal server error" },
+			{ status: 500 }
+		);
+	}
+}
