ce7cccf46c0d7c6bbfb35525309144d9ebc98244
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 10:58:53 +0600

    feat: add heading class component

--- src/components/home/explore-menu.jsx
+++ src/components/home/explore-menu.jsx
@@ -4,9 +4,7 @@ import { MenuList } from "./menu-list";
 export default function ExploreMenu() {
 	return (
 		<section className="space-y-8">
-			<h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl lg:6xl">
-				Explore our menu
-			</h2>
+			<h2 className="heading">Explore our menu</h2>
 			<p className="text-gray-700">
 				Chose from a diverse menu featuring a delectable array of
 				dishes. Our mission is to satisfy your carvings and elevate your
