c7bf56038351c88da01494c37cd19cb6c719aba4
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 09:34:39 +0600

    feat: create menu-list file for providing the list of menus

---
+++ src/lib/menu-list.js
@@ -0,0 +1,34 @@
+export const menu_list = [
+	{
+		menu_name: "Salad",
+		menu_image: "/images/menu_1.png",
+	},
+	{
+		menu_name: "Rolls",
+		menu_image: "/images/menu_2.png",
+	},
+	{
+		menu_name: "Deserts",
+		menu_image: "/images/menu_3.png",
+	},
+	{
+		menu_name: "Sandwich",
+		menu_image: "/images/menu_4.png",
+	},
+	{
+		menu_name: "Cake",
+		menu_image: "/images/menu_5.png",
+	},
+	{
+		menu_name: "Pure Veg",
+		menu_image: "/images/menu_6.png",
+	},
+	{
+		menu_name: "Pasta",
+		menu_image: "/images/menu_7.png",
+	},
+	{
+		menu_name: "Noodles",
+		menu_image: "/images/menu_8.png",
+	},
+];
