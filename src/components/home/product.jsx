64b592d66aa65272b465f974619d91d3c1e4c2b5
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 22:24:09 +0600

    feat: add cart buttons

--- src/components/home/product.jsx
+++ src/components/home/product.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import CartButtons from "./cart-buttons";
 
 export default function Product({ product }) {
 	return (
 		<article className="rounded-xl overflow-hidden bg-white border-2 border-gray-200 shadow hover:scale-102 hover:shadow-lg hover:border-emerald-500/80 transition transform duration-300">
-			<div className="w-full">
+			<div className="w-full relative">
 				<Image
 					src={product.image}
 					width={200}
@@ -11,6 +12,7 @@ export default function Product({ product }) {
 					alt={product.name}
 					className="w-full object-cover aspect-[4/3]"
 				/>
+				<CartButtons product={product} />
 			</div>
 			<div className="p-4 space-y-2.5">
 				<h4 className="font-medium md:text-lg">{product.name}</h4>
