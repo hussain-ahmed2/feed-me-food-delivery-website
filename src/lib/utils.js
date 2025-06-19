bc394e877960cde69b11ffe1218c5c00da38ac03
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 09:16:14 +0600

    feat: create util file for some utility function

---
+++ src/lib/utils.js
@@ -0,0 +1,29 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export async function hashPassword(password) {
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+	return hashedPassword;
+}
+
+export async function checkPasswordMatch(password, hashedPassword) {
+	const isMatched = await bcrypt.compare(password, hashedPassword);
+	return isMatched;
+}
+
+export function generateToken(payload) {
+	const token = jwt.sign(payload, process.env.JWT_SECRET);
+	return token;
+}
+
+export function decodeToken(token) {
+	const payload = jwt.verify(token, process.env.JWT_SECRET);
+	return payload;
+}
+
+export async function getUser() {
+	const response = await fetch("/api/auth/user");
+	const data = await response.json();
+	return data;
+}
