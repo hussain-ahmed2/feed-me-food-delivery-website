43a17773a204a9dbb92dcfed5be6d71dbda3ff2b
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 09:33:37 +0600

    feat: create explore menu component along with menulist component

---
+++ src/components/home/explore-menu.jsx
@@ -0,0 +1,23 @@
+import { menu_list } from "@/lib/menu-list";
+import { MenuList } from "./menu-list";
+
+export default function ExploreMenu() {
+	return (
+		<section className="space-y-8">
+			<h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl lg:6xl">
+				Explore our menu
+			</h2>
+			<p className="text-gray-700">
+				Chose from a diverse menu featuring a delectable array of
+				dishes. Our mission is to satisfy your carvings and elevate your
+				dining experience, one delicious meal at a time.
+			</p>
+
+			<ul className="flex items-center justify-between gap-4 md:gap-8 overflow-x-auto scrollbar-hide">
+				{menu_list.map((menu) => (
+					<MenuList key={menu.menu_name} {...menu} />
+				))}
+			</ul>
+		</section>
+	);
+}
---
+++ src/components/home/menu-list.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useShop } from "@/context/shop-context";
+import Image from "next/image";
+
+export function MenuList({ menu_image, menu_name }) {
+	const { category, pickCategory } = useShop();
+
+	return (
+		<li
+			onClick={() => pickCategory(menu_name)}
+			className="space-y-3 text-center"
+		>
+			<div
+				className={`w-30 md:w-50 h-30 md:h-50 ring-emerald-500 m-1 rounded-full transition duration-300 ${
+					category === menu_name ? "ring-4 shadow-xl" : ""
+				}`}
+			>
+				<Image
+					className="w-full aspect-square"
+					src={menu_image}
+					width={200}
+					height={200}
+					alt={menu_name}
+				/>
+			</div>
+			<h4
+				className={`font-semibold md:text-lg transition-colors duration-300 ${
+					category === menu_name ? "text-emerald-500" : ""
+				}`}
+			>
+				{menu_name}
+			</h4>
+		</li>
+	);
+}
