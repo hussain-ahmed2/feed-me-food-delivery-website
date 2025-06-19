34cb9093bd5d7f1c42fadcb7d8df1a043b41be32
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 22:21:05 +0600

    feat: add dishes grid

--- src/app/dishes/page.js
+++ src/app/dishes/page.js
@@ -1,3 +1,11 @@
+import DishesGrid from "@/components/dishes/dishes-grid";
+
 export default function DishesPage() {
-	return <div className="page">DishesPage</div>;
+	return (
+		<div className="page space-y-10">
+			<h2 className="heading">All Dishes</h2>
+
+			<DishesGrid />
+		</div>
+	);
 }
