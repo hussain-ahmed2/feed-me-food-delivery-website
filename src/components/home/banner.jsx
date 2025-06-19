b47229bfb257335157084b8ec1a585c5d084addc
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 10:57:34 +0600

    style: adjust banner styling

--- src/components/home/banner.jsx
+++ src/components/home/banner.jsx
@@ -10,11 +10,11 @@ export default function Banner() {
 				width={1000}
 				alt="banner-image"
 			/>
-			<div className="absolute z-10 bottom-0 left-0 m-8 lg:m-16 text-white space-y-10">
-				<h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl lg:6xl">
+			<div className="absolute z-10 bottom-0 left-0 m-8 lg:m-16 text-white space-y-5 max-w-xl">
+				<h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-6xl">
 					Order your favorite food here
 				</h2>
-				<p className="hidden md:block max-w-xl">
+				<p className="hidden md:block">
 					Chose from a diverse menu featuring a delectable array of
 					dishes crafted with the finest ingredients and culinary
 					expertise. Our mission is to satisfy your carvings and
