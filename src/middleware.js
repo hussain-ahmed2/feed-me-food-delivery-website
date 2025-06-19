cced6d8de36930200a0afe2b316dc46baf24b29c
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 09:29:04 +0600

    feat: create middleware for protected routes

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+	const tokenCookie = request.cookies.get("token");
+	const token = tokenCookie ? tokenCookie.value : null;
+
+	const protectedPaths = ["/login", "/register"];
+
+	if (!token && request.nextUrl.pathname.startsWith("/profile")) {
+		return NextResponse.redirect(new URL("/login", request.url));
+	}
+
+	if (token && protectedPaths.includes(request.nextUrl.pathname)) {
+		return NextResponse.redirect(new URL("/profile", request.url));
+	}
+
+	return NextResponse.next();
+}
