0a3f9db16ce49a00e5016ede5cbff0801b658a1f
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-19 12:43:11 +0600

    feat: add top-dishes component to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Banner from "@/components/home/banner";
 import Menu from "@/components/home/explore-menu";
+import TopDishes from "@/components/home/top-dishes";
 
 export default function Home() {
 	return (
 		<div className="page space-y-10">
 			<Banner />
 			<Menu />
+			<TopDishes />
 		</div>
 	);
 }
