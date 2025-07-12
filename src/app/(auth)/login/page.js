3c5b52f28ff1902ba3eb4792d26a84a69a5eaa17
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-07-12 09:49:47 +0600

    use auth context login and register function to authenticate

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -4,10 +4,11 @@ import InputField from "@/components/input-field";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useActionState, useRef } from "react";
-import { login } from "@/lib/utils";
+import { useAuth } from "@/context/AuthContext";
 
 export default function LoginPage() {
 	const router = useRouter();
+	const { login } = useAuth();
 
 	const action = async (prevState, formData) => {
 		const form = Object.fromEntries(formData.entries());
@@ -40,22 +41,10 @@ export default function LoginPage() {
 	return (
 		<section className="page justify-center items-center">
 			<div className="w-full max-w-2xl">
-				<h1 className="font-black text-center text-3xl md:text-4xl mb-4">
-					Login
-				</h1>
-				<p className="text-center font-medium text-lg text-gray-700 mb-6">
-					Login in to your account
-				</p>
+				<h1 className="font-black text-center text-3xl md:text-4xl mb-4">Login</h1>
+				<p className="text-center font-medium text-lg text-gray-700 mb-6">Login in to your account</p>
 				<form action={formAction} className="flex flex-col gap-3">
-					<InputField
-						name="email"
-						label="Email:"
-						type="email"
-						placeholder="Enter your email"
-						defaultValue={state.form?.email}
-						error={state.errors?.email}
-						ref={inputRefs}
-					/>
+					<InputField name="email" label="Email:" type="email" placeholder="Enter your email" defaultValue={state.form?.email} error={state.errors?.email} ref={inputRefs} />
 					<InputField
 						name="password"
 						label="Password:"
@@ -65,22 +54,13 @@ export default function LoginPage() {
 						error={state.errors?.password}
 						ref={inputRefs}
 					/>
-					<button
-						disabled={isPending}
-						type="submit"
-						className={`btn-success ${
-							isPending ? "cursor-progress opacity-70" : ""
-						}`}
-					>
+					<button disabled={isPending} type="submit" className={`btn-success ${isPending ? "cursor-progress opacity-70" : ""}`}>
 						Login
 					</button>
 				</form>
 				<p className="mt-3 text-center">
 					don&#39;t have an account?{" "}
-					<Link
-						className="font-medium underline hover:text-emerald-500"
-						href="/register"
-					>
+					<Link className="font-medium underline hover:text-emerald-500" href="/register">
 						Register
 					</Link>
 				</p>
--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -4,10 +4,11 @@ import InputField from "@/components/input-field";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useActionState, useRef } from "react";
-import { register } from "@/lib/utils";
+import { useAuth } from "@/context/AuthContext";
 
 export default function RegisterPage() {
 	const router = useRouter();
+	const { register } = useAuth();
 
 	const action = async (prevState, formData) => {
 		const form = Object.fromEntries(formData.entries());
@@ -38,30 +39,11 @@ export default function RegisterPage() {
 	return (
 		<section className="page justify-center items-center">
 			<div className="w-full max-w-2xl">
-				<h1 className="font-black text-center text-3xl md:text-4xl mb-4">
-					Register
-				</h1>
-				<p className="text-center font-medium text-lg text-gray-700 mb-6">
-					Create a new account here
-				</p>
+				<h1 className="font-black text-center text-3xl md:text-4xl mb-4">Register</h1>
+				<p className="text-center font-medium text-lg text-gray-700 mb-6">Create a new account here</p>
 				<form action={formAction} className="flex flex-col gap-3">
-					<InputField
-						name="name"
-						label="Name:"
-						placeholder="Enter your name"
-						defaultValue={state.form?.name}
-						error={state.errors?.name}
-						ref={inputRefs}
-					/>
-					<InputField
-						name="email"
-						label="Email:"
-						type="email"
-						placeholder="Enter your email"
-						defaultValue={state.form?.email}
-						error={state.errors?.email}
-						ref={inputRefs}
-					/>
+					<InputField name="name" label="Name:" placeholder="Enter your name" defaultValue={state.form?.name} error={state.errors?.name} ref={inputRefs} />
+					<InputField name="email" label="Email:" type="email" placeholder="Enter your email" defaultValue={state.form?.email} error={state.errors?.email} ref={inputRefs} />
 					<InputField
 						name="password"
 						label="Password:"
@@ -80,22 +62,13 @@ export default function RegisterPage() {
 						error={state.errors?.confirmPassword}
 						ref={inputRefs}
 					/>
-					<button
-						disabled={isPending}
-						type="submit"
-						className={`btn-success ${
-							isPending ? "cursor-progress opacity-70" : ""
-						}`}
-					>
+					<button disabled={isPending} type="submit" className={`btn-success ${isPending ? "cursor-progress opacity-70" : ""}`}>
 						Register
 					</button>
 				</form>
 				<p className="mt-3 text-center">
 					already have an account?{" "}
-					<Link
-						className="font-medium underline hover:text-emerald-500"
-						href="/login"
-					>
+					<Link className="font-medium underline hover:text-emerald-500" href="/login">
 						Login
 					</Link>
 				</p>
